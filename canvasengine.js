64ad21451290dff05438f5ca6fa773e1c65d1e1c
Miloshcavitch <miloshcavitch@gmail.com>
2016-02-27 13:29:17 -0500

    adds renderUI function

--- canvasengine.js
+++ canvasengine.js
@@ -37,16 +37,38 @@ var renderPoly = function(shape){
   ctx.closePath();
   ctx.fillStyle = 'red';
   ctx.fill();
-  if (shape.editPoints === true){
-    shape.positions.forEach(function(el){
-      ctx.beginPath();
-      ctx.globalAlpha = 0.6
-      ctx.arc(el.worldX, el.worldY, 10, 0, 2 * Math.PI);
-      ctx.fillStyle = 'gray';
-      ctx.fill();
-    });
-    ctx.globalAlpha = 1;
-  }
+}
+var polyEditPoint = function(shape){
+  ctx.globalAlpha = 0.6;
+  shape.positions.forEach(function(p){
+    ctx.beginPath();
+    ctx.arc(p.worldX, p.worldY, 10, 0, 2 * Math.PI);
+    ctx.fillStyle = 'gray';
+    ctx.fill();
+  });
+  ctx.beginPath();
+  ctx.globalAlpha = 0.4;
+  ctx.moveTo(shape.positions[0].worldX, shape.positions[0].worldY);
+  shape.positions.forEach(function(p){
+    ctx.lineTo(p.worldX, p.worldY);
+  });
+  ctx.lineTo(shape.positions[0].worldX, shape.positions[0].worldY);
+  ctx.strokeStyle = 'gray';
+  ctx.lineWidth = 1;
+  ctx.stroke();
+  ctx.globalAlpha = 1;
+}
+
+var renderUI = function(){
+  pseudoSprite.shapes.forEach(function(el){
+    if (el.editPoints === true){
+      switch(el.type){
+        case 'polygon':
+          polyEditPoint(el);
+      }
+    }
+
+  });
 }
 ////////////////////////////////////////////
 ///////////////////////////////////////////
@@ -121,4 +143,5 @@ var activeRenderLayer = function(){
 var update = function(){
   activeUpdate();
   render();
+  renderUI();
 }
