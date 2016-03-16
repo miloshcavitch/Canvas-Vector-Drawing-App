a20d82fe38e762369dad05846a5b6cd44ec7f675
Miloshcavitch <miloshcavitch@gmail.com>
2016-03-16 13:56:39 -0400

    checking the positive or negative slope of all lines in poly shape now works

--- linesnap.js
+++ linesnap.js
@@ -1,32 +1,28 @@
 var polyLineSnap = function(){
   var candidate = {worldX: undefined, worldY: undefined, type: 'line'};
   pseudoSprite.shapes.forEach(function(s){
-    if (s.type === 'polygon'){
-      for (var i = 0; i < s.positions.length; i++){
-        var rise, run, slope;
-        if (s.positions.length >= (i + 1)){
-          rise = Math.abs(s.positions[i].worldY - s.positions[i+1].worldY);
-          run = Math.abs(s.positions[i].worldX - s.positions[i+1].worldX);
-          if (s.positions[i].worldX < s.positions[i+1].worldX){
-            if (s.positions[i].worldY > s.positions[i+1].worldY){// upwards, according to standard coordinate system
-              rise = rise;
-            } else {
-              rise = rise * -1;
-            }
-          }
-        } else {
-          rise = Math.abs(s.positions[i].worldY - s.positions[0].worldY);
-          run = Math.abs(s.positions[i].worldX - s.positions[0].worldX);
-          if (s.positions[i].worldX < s.positions[0].worldX){
-            if (s.positions[i].worldY > s.positions[0].worldY){// upwards, according to standard coordinate system
-              rise = rise;
-            } else {
-              rise = rise * -1;
-            }
-          }
-        }
+    for (var i = 0; i < s.positions.length; i++){
+      var rise, run, slope, inverse, special;
+
+
+      if (s.positions.length <= (i + 1)){
+        rise = Math.abs(s.positions[i].worldY - s.positions[0].worldY);
+        run = Math.abs(s.positions[i].worldX - s.positions[0].worldX);
         slope = rise/run;
+        if ((s.positions[i].worldX > s.positions[0].worldX && s.positions[i].worldY < s.positions[0].worldY) || (s.positions[i].worldX < s.positions[0].worldX && s.positions[i].worldY > s.positions[0].worldY)){
+          slope = slope * -1;
+        }
+      } else {
+        rise = Math.abs(s.positions[i].worldY - s.positions[i+1].worldY);
+        run = Math.abs(s.positions[i].worldX - s.positions[i+1].worldX);
+        slope = rise/run;
+        if ((s.positions[i].worldX > s.positions[i+1].worldX && s.positions[i].worldY < s.positions[i+1].worldY) || (s.positions[i].worldX < s.positions[i+1].worldX && s.positions[i].worldY > s.positions[i+1].worldY)){
+          slope = slope * -1;
+        }
       }
+
+
+      console.log(rise + "over " + run +  "signed slope equals " + slope);
     }
   });
 }
