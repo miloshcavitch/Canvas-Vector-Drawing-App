c00b500e8dbada86e6d97471082cbc091983f04d
Miloshcavitch <miloshcavitch@gmail.com>
2016-03-27 18:53:53 -0400

    DRYification works. backing up again for more

--- linesnap.js
+++ linesnap.js
@@ -8,28 +8,11 @@ var polyLineSnap = function(){
 
 
         if (s.positions.length <= (i + 1)){
-          rise = Math.abs(s.positions[i].worldY - s.positions[0].worldY);
-          run = Math.abs(s.positions[i].worldX - s.positions[0].worldX);
-          slope = rise/run;
-          if ((s.positions[i].worldX > s.positions[0].worldX && s.positions[i].worldY < s.positions[0].worldY) || (s.positions[i].worldX < s.positions[0].worldX && s.positions[i].worldY > s.positions[0].worldY)){
-            slope = slope * -1;
-          }
-          inverse = -1/slope;
-          leftY = inverse * (0 - mouseX) + mouseY;
-          rightY = inverse * (canvas.width - mouseX) + mouseY;
-          possibleIntersections = checkLineIntersection(s.positions[i].worldX, s.positions[i].worldY, s.positions[0].worldX, s.positions[0].worldY, 0, leftY, canvas.width, rightY);
+          possibleIntersections = calculationOfLine(s.positions[i].worldX, s.positions[i].worldY, s.positions[0].worldX, s.positions[0].worldY);
         } else {
-          rise = Math.abs(s.positions[i].worldY - s.positions[i+1].worldY);
-          run = Math.abs(s.positions[i].worldX - s.positions[i+1].worldX);
-          slope = rise/run;
-          if ((s.positions[i].worldX > s.positions[i+1].worldX && s.positions[i].worldY < s.positions[i+1].worldY) || (s.positions[i].worldX < s.positions[i+1].worldX && s.positions[i].worldY > s.positions[i+1].worldY)){
-            slope = slope * -1;
-          }
-          inverse = -1/slope;
-          leftY = inverse * (0 - mouseX) + mouseY;
-          rightY = inverse * (canvas.width - mouseX) + mouseY;
-          possibleIntersections = checkLineIntersection(s.positions[i].worldX, s.positions[i].worldY, s.positions[i+1].worldX, s.positions[i+1].worldY, 0, leftY, canvas.width, rightY);
+          possibleIntersections = calculationOfLine(s.positions[i].worldX, s.positions[i].worldY, s.positions[i+1].worldX, s.positions[i+1].worldY);
         }
+
         if (possibleIntersections.onLine1 === true && possibleIntersections.onLine2 === true){
           var tempPoint = {worldX: possibleIntersections.x, worldY: possibleIntersections.y,};
           if (pythagLength(mouseX, mouseY, tempPoint) <= tempLength){
@@ -45,7 +28,27 @@ var polyLineSnap = function(){
     return candidate;
   }
 }
+var linePolyTest = function(s){
+  for (var i = 0; i < s.positions.length; i++){
+    var rise, run, slope, inverse, special, leftY, rightY, possibleIntersections;
 
+
+    if (s.positions.length <= (i + 1)){
+      possibleIntersections = calculationOfLine(s.positions[i].worldX, s.positions[i].worldY, s.positions[0].worldX, s.positions[0].worldY);
+    } else {
+      possibleIntersections = calculationOfLine(s.positions[i].worldX, s.positions[i].worldY, s.positions[i+1].worldX, s.positions[i+1].worldY);
+    }
+
+    if (possibleIntersections.onLine1 === true && possibleIntersections.onLine2 === true){
+      var tempPoint = {worldX: possibleIntersections.x, worldY: possibleIntersections.y,};
+      if (pythagLength(mouseX, mouseY, tempPoint) <= tempLength){
+        tempLength = pythagLength(mouseX, mouseY, tempPoint);
+        candidate.worldX = possibleIntersections.x;
+        candidate.worldY = possibleIntersections.y;
+      }
+    }
+  }
+}
 var calculationOfLine = function(x1, y1, x2, y2){
   rise = Math.abs(y1 - y2);
   run = Math.abs(x1 - x2);
@@ -56,8 +59,10 @@ var calculationOfLine = function(x1, y1, x2, y2){
   inverse = -1/slope;
   leftY = inverse * (0 - mouseX) + mouseY;
   rightY = inverse * (canvas.width - mouseX) + mouseY;
-  possibleIntersections = checkLineIntersection(x1, y1, x2, y2, 0, leftY, canvas.width, rightY);
+  var intersections = checkLineIntersection(x1, y1, x2, y2, 0, leftY, canvas.width, rightY);
+  return intersections;
 }
+
 var mirrorLineSnap = function(){
 
 }
